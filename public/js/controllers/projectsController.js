22285ffceaf95ce6d072c70bb81c3af804bf2518
Matthew D Diaz <matthewdiaz10@ufl.edu>
2015-08-22 23:35:26 -0400

    Modify separtion of each project container. Using bootstrap col-xs-4 for more uniform spacing of containers

--- public/js/controllers/projectsController.js
+++ public/js/controllers/projectsController.js
@@ -1,5 +1,5 @@
 angular.module('siteM.projectsController', ['ngRoute'])
   .controller('ProjectsController', function(){
     this.projectImages = ['calculator.jpg', 'dictionaryApp.jpg',
-                          'internetPermission.jpg', 'NameThatCountry.jpg'];
+                          'internetPermission.jpg'];
   });
