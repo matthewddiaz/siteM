d17b4915dd4f44c20b001a7edc7ca91e8e0b0698
Matthew D Diaz <matthewdiaz10@ufl.edu>
2015-08-23 11:52:09 -0400

    Fix toogle effect of Bio/Tech in home.html. Will not toggle unless the opposite skill was showing

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -1,7 +1,22 @@
 angular.module('siteM.homeController', ['ngRoute'])
   .controller('HomeController', function(){
-    this.skill = true;
-    this.toggleSkill = function(){
-      this.skill = !this.skill;
+    this.isBio = true;
+    var controller = this;
+    this.showBio = function(){
+      console.log($( "#Tech" ).hasClass( "on" ));
+      if($( "#Tech" ).hasClass( "on" ) ){
+        $( "#Tech" ).removeClass("on").addClass("off");
+        $( "#Bio" ).removeClass("off").addClass("on");
+        controller.isBio = !controller.isBio;
+      }
+    }
+
+    this.showTech = function(){
+      console.log($( "#Bio" ).hasClass( "on" ));
+      if($( "#Bio" ).hasClass( "on" ) ){
+        $( "#Bio" ).removeClass("on").addClass("off");
+        $( "#Tech" ).removeClass("off").addClass("on");
+        controller.isBio = !controller.isBio;
+      }
     }
   });
