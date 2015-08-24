02abdfbc5350cc9e687baa7408926b4a05b74562
Matthew D Diaz <matthewdiaz10@ufl.edu>
2015-08-24 18:44:03 -0400

    Modify project.html. Rows now contain at most three images. When a row is filled a new row is created. Padding was added to each row

---
+++ public/js/controllers/popUpController.js
@@ -0,0 +1,5 @@
+angular.module('siteM.popUpController', ['ngRoute'])
+  .controller('popUpController', function(){
+    this.projectImages = ['calculator.jpg', 'dictionaryApp.jpg',
+                          'internetPermission.jpg'];
+  });
--- public/js/controllers/projectsController.js
+++ public/js/controllers/projectsController.js
@@ -1,5 +1,14 @@
 angular.module('siteM.projectsController', ['ngRoute'])
   .controller('ProjectsController', function(){
     this.projectImages = ['calculator.jpg', 'dictionaryApp.jpg',
-                          'internetPermission.jpg'];
+                          'internetPermission.jpg', 'NameThatCountry.jpg',
+                          'great-wave.jpg', 'great-owl.jpg', 'matt.jpg'
+                         ];
+    this.projectRows = [];
+    this.rowLength = 3;
+
+    for(var i = 0; i < this.projectImages.length; i = i + this.rowLength){
+      this.projectRows.push(this.projectImages.slice(i,i + this.rowLength));
+    }
+    console.log(this.projectRows);
   });
