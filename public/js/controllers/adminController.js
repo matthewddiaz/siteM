08a04b8ec6a0cae1527a3a9563d9667bca56285d
Matthew D Diaz <matthewdiaz10@ufl.edu>
2015-09-21 11:04:27 -0400

    Modify admin.html if uploaded was submitted successfully all the values in the form will be set the pristine/original form

--- public/js/controllers/adminController.js
+++ public/js/controllers/adminController.js
@@ -1,10 +1,13 @@
 angular.module('siteM.adminController', [ 'ngRoute', 'ngFileUpload'])
   .controller('AdminController', ['$scope', '$http', 'Upload', '$timeout',
               function($scope, $http, Upload, $timeout){
-    this.projectName;
-    this.github;
-    this.projectDescription;
-    var controller = this;
+    $scope.formProperties = {
+      projectName: "",
+      github: "",
+      projectDescription: "",
+      picFile: ""
+    }
+    var originalForm = angular.copy($scope.formProperties);
 
     $scope.uploadPic = function(file) {
       var  projectInfo = {
@@ -14,15 +17,17 @@ angular.module('siteM.adminController', [ 'ngRoute', 'ngFileUpload'])
         url: 'data/uploadDocs',
         method: 'POST',
         fields: {
-          "projectName" : controller.projectName,
-          "projectUrl" : controller.github,
-          "projectDescription" : controller.projectDescription,
+          "projectName" : $scope.formProperties.projectName,
+          "projectUrl" : $scope.formProperties.github,
+          "projectDescription" : $scope.formProperties.projectDescription,
           "imageType" : file.type
         }, // Any data needed to be submitted along with the files
         file: file
       }).success(function(data, status, headers, config) {
         if(data.ok){
           toastr.success("Upload Complete", "Success!");
+           $scope.adminForm.$setPristine();
+           $scope.formProperties = angular.copy(originalForm);
         }else{
           toastr.error('Could not Submit', 'Error!');
         }
